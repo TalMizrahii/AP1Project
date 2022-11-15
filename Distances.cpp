23cc550887bda7b2f2cd8b3a114a30b56c0ad0c7
TalMizrahii <Taltalon1927@gmail.com>
2022-11-15 18:44:44 +0200

    Created the chebyshevDistance function - NOT TESTED.

--- Distances.cpp
+++ Distances.cpp
@@ -28,6 +28,7 @@ double Distances::euclideanDistance(vector<double> v1, vector<double> v2) {
     // Returning the square root of the result.
     return sqrt(result);
 }
+
 /**
  * Calculating the Taxicab Geometry distance between two vectors.
  * @param v1 the first vector.
@@ -38,7 +39,7 @@ double Distances::taxicabDistance(vector<double> v1, vector<double> v2) {
     // Setting the result to 0.
     double result = 0;
     // Iterating over the indexes in each vector.
-    for (int i = 0; i < v1.size(); i++){
+    for (int i = 0; i < v1.size(); i++) {
         // Calculating the subtraction between them in absolute value and adding to result.
         result += fabs(v1[i] - v2[i]);
     }
@@ -46,9 +47,26 @@ double Distances::taxicabDistance(vector<double> v1, vector<double> v2) {
     return result;
 }
 
-
+/**
+ * Calculating the Chebyshev distance between two vectors.
+ * @param v1 the first vector.
+ * @param v2 the second vector.
+ * @return the Chebyshev distance between the two vectors.
+ */
 double Distances::chebyshevDistance(vector<double> v1, vector<double> v2) {
-
+    // Setting the maximum to the first couple of points.
+    double max = fabs(v1[0] - v2[0]);
+    double temp;
+    // Iterating over the indexes in each vector.
+    for (int i = 1; i < v1.size(); i++) {
+        // Setting the temp to be the value of distance between v1[i] to v2[i].
+        temp = fabs(v1[0] - v2[0]);
+        // If temp is bigger than max, max is now temp.
+        if (temp > max)
+            max = temp;
+    }
+    // Return the maximum value.
+    return max;
 }
 
 double Distances::canberraDistance(vector<double> v1, vector<double> v2) {
