ddb84ef5536cf31c62537c21a18ca183e8a9bdf4
yuval <Arbelyuval56@gmail.com>
2022-11-15 20:56:16 +0200

    Version 3 not fully tested.

--- main.cpp
+++ main.cpp
@@ -10,33 +10,42 @@
 
 using namespace std;
 
-
+/**
+ * Checking if the user input is in the right format.
+ * @param s a string that the user entered.
+ * @return boolean, True if the the string in the right format False otherwise.
+ */
 bool isNumber(string s) {
+    // For loop that check each char of the user input.
     for (int i = 0; i < s.length(); i++) {
+        // Check if the first char in the input is ' '.
         if (isdigit(s[0]) == false) {
             return false;
         }
+        // Checking if the user input has 2 spaces neighbors.
         if (i > 0 && (s[i] == ' ' && s[i - 1] == ' ')) {
             return false;
         }
+        // Checking if there ' ' , if we have only 1 ' ' the format is still correct.
         if (s[i] == ' ') {
             continue;
         }
+        // Checking if the user input a valid floating point number.
         if (s[i] == '.') {
+            // If we have '.' we must get a number in the next char, otherwise not in format.
             if (isdigit(s[i + 1]) == false) {
                 return false;
             }
             continue;
         }
-
-
+        // Checking if the char is a digit.
         if (isdigit(s[i]) == false)
             return false;
     }
     return true;
 }
 
-
+//DELETE
 void print(std::vector<double> const &vector) {
     for (int i = 0; i < vector.size(); i++) {
         std::cout << vector.at(i) << ' ';
@@ -45,40 +54,23 @@ void print(std::vector<double> const &vector) {
 
 }
 
+
 /**
-vector<double> insert_To_Vector() {
-    double input;
-    vector<double> vector;
-
-    // user can add element as much as they want.
-    while (cin >> input) {
-
-        if (input == '32') {
-            continue;
-        }
-
-        //checking if the user input is a digit or not.
-        if (!isdigit((int) input)) {
-            cout << "Illegal input try again!" << endl;
-            continue;
-        }
-        // adding the checked input into the vector.
-        vector.push_back(input);
-    }
-    return vector;
-};
- **/
-
-
+ *
+ * @return Return a valid vector by the specified format of the task.
+ */
 vector<double> insert_To_Vector1() {
     string input;
     double number;
     vector<double> vector;
+    // Using a stream function to get a string from the user
     getline(cin, input);
+    // Send the string to a function that check is validation.
     if (!isNumber(input)) {
         cout << "Illegal format" << endl;
         exit(0);
     }
+    // Making a vector from the user input string.
     istringstream vectorStream(input);
     while (vectorStream >> number) {
         vector.push_back(number);
@@ -88,19 +80,26 @@ vector<double> insert_To_Vector1() {
 
 };
 
-
+/**
+ *
+ * @return return 0 if the program run without issues.
+ */
 int main() {
     vector<double> vector1;
     vector<double> vector2;
 
+    // Making a vectors from the user inputs
     vector1 = insert_To_Vector1();
     vector2 = insert_To_Vector1();
+    //DELETE
     print(vector1);
     print(vector2);
+    // Checking if the vectors have the same size.
     if (vector1.size() != vector2.size()) {
         cout << "The vectors is not the same size!" << endl;
         return 0;
     }
+    //DELETE
     print(vector1);
     print(vector2);
 
