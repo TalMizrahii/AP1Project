521b3de022f3ab72447f647d6b788d0e7fa660ac
TalMizrahii <Taltalon1927@gmail.com>
2022-11-20 17:26:30 +0200

    Updated makefile and bug fix in chebyshevDistance

--- Distances.cpp
+++ Distances.cpp
@@ -40,7 +40,7 @@ double Distances::chebyshevDistance(vector<double> v1, vector<double> v2) {
     // Iterating over the indexes in each vector.
     for (unsigned long i = 1; i < v1.size(); i++) {
         // Setting the temp to be the value of distance between v1[i] to v2[i].
-        temp = fabs(v1[0] - v2[0]);
+        temp = fabs(v1[i] - v2[i]);
         // If temp is bigger than max, max is now temp.
         if (temp > max)
             max = temp;
